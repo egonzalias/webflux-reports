b4ea743d3fd18506a1b3a3747c5aa7cefca7bf54
egonzalias <edwin.gonzalias@globant.com>
2025-09-16 10:31:42 -0500

    Adding new bean with profile docker to be able to run the container in docker environment

--- infrastructure/driven-adapters/dynamo-db/src/main/java/co/com/crediya/dynamodb/config/DynamoDBConfig.java
+++ infrastructure/driven-adapters/dynamo-db/src/main/java/co/com/crediya/dynamodb/config/DynamoDBConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
@@ -34,6 +35,19 @@ public class DynamoDBConfig {
     public DynamoDbAsyncClient amazonDynamoDBAsync(MetricPublisher publisher, @Value("${aws.region}") String region) {
         return DynamoDbAsyncClient.builder()
                 .credentialsProvider(WebIdentityTokenFileCredentialsProvider.create())
+                .region(Region.of(region))
+                .overrideConfiguration(o -> o.addMetricPublisher(publisher))
+                .build();
+    }
+
+    @Bean
+    @Profile("docker")
+    public DynamoDbAsyncClient amazonDynamoDBDocker(
+            @Value("${aws.region}") String region,
+            MetricPublisher publisher) {
+
+        return DynamoDbAsyncClient.builder()
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
                 .region(Region.of(region))
                 .overrideConfiguration(o -> o.addMetricPublisher(publisher))
                 .build();
