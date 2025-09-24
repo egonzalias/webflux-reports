1924c592ca5686f6ddc8af13c85003899cb6780a
egonzalias <edwin.gonzalias@globant.com>
2025-09-24 15:49:38 -0500

    Adding documentation

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/Handler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/Handler.java
@@ -1,6 +1,13 @@
 package co.com.crediya.api;
 
+import co.com.crediya.api.exception.ErrorResponse;
+import co.com.crediya.model.report.Report;
 import co.com.crediya.usecase.report.ReportUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -17,6 +24,38 @@ public class Handler {
     private final ReportUseCase reportUseCase;
     private final Validator validator;
 
+    @Operation(
+            summary = "Obtiene el reporte de préstamos aprobados",
+            description = "Este endpoint retorna el reporte con el número total de préstamos aprobados y su monto total.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Reporte de préstamos aprobados",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Report.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Parámetros inválidos o error en la consulta",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Acceso prohibido"
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Acceso denegado o rol invalido para realizar esta accion."
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Error interno",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    )
+            }
+    )
     public Mono<ServerResponse> getTotalApprovedRequests(ServerRequest serverRequest) {
         return reportUseCase.getApprovedLoansCountByReportId("approved-loans")
                 .flatMap(report ->
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/RouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/RouterRest.java
@@ -1,5 +1,7 @@
 package co.com.crediya.api;
 
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -10,6 +12,13 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 @Configuration
 public class RouterRest {
     @Bean
+    @RouterOperations({
+            @RouterOperation(
+                    path = "/api/v1/reportes",
+                    beanClass = Handler.class,
+                    beanMethod = "getTotalApprovedRequests"
+            )
+    })
     public RouterFunction<ServerResponse> reportsRoutes(Handler handler) {
         return route()
                 .GET("/api/v1/reportes", handler::getTotalApprovedRequests)
